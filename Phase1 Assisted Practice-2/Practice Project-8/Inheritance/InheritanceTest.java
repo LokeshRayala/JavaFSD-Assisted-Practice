dc7f2c5e8bc559891ad77cba14fc40f8a100ac68
Lokesh Rayala <72343510+LokeshRayala@users.noreply.github.com>
2022-07-25 10:20:50 +0530

    Inheritance OOPs

---
+++ Phase1 Assisted Practice-2/Practice Project-8/Inheritance/InheritanceTest.java
@@ -0,0 +1,18 @@
+package inheritanceDemo;
+
+public class InheritanceTest {
+	
+	
+		
+		//inheritance in java  help for code  reusability
+		//moreover we can prepare new methods which are  not available in parent class also
+		
+		// 
+		public static void main(String[] args) {
+			
+			MountainBike mb=  new MountainBike(25, 100, 20);
+			System.out.println("Details: "+mb);
+
+		}
+
+}
