ceb250fccb2e62c6d14f9d65b3b285f374c7682d
Lokesh Rayala <72343510+LokeshRayala@users.noreply.github.com>
2022-07-26 13:30:55 +0530

    Java Phase-1 Assisted Practice-3

---
+++ Phase1 Assisted Practice-3/Practice Project-1/ArrayRotationDemo.java
@@ -0,0 +1,49 @@
+package dataStructures;
+
+public class ArrayRotationDemo {
+	
+	public void rotate(int num[] ,int k) {
+		
+		if(k> num.length) {
+			
+			k= k% num.length;  
+			
+			int result[] = new int[num.length];
+			
+			
+			for(int i=0; i<k; i++) {
+				
+				result[i]= num[num.length-k+i]; 
+				
+			}
+			
+			int j=0;
+			
+			for(int i=k; i<num.length;i++) {
+				
+				result[i]=num[j];
+				j++;
+			}
+			
+			System.arraycopy(result, 0, num, 0, num.length);
+			
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayRotationDemo r= new ArrayRotationDemo();
+		
+		int arr[]= {1,2,3,4,5,6};
+		
+		
+		r.rotate(arr, 8); 
+		
+		for (int i=0; i<arr.length;i++) {
+			System.out.print(arr[i]+ " ");
+		}
+
+	}
+}
---
+++ Phase1 Assisted Practice-3/Practice Project-2/OrderStatistics.java
@@ -0,0 +1,26 @@
+package dataStructures;
+
+
+import java.util.Arrays;
+
+public class OrderStatistics {
+	
+	public static int KthSmallest(Integer[] arr,int k) {
+		
+		Arrays.sort(arr);
+		
+		
+		return arr[k-1]; 
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Integer arr[]= new Integer[] {22,55,88,77,44,99,33,11,66};
+		
+		int k=4;  //finding fourth smallest element in an unsorted array
+		
+		System.out.println("Kth Smallest Element is : "+ KthSmallest(arr, k) );
+	}
+
+}
