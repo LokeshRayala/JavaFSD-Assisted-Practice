ad513fe28eb8df71ff3c925cc08868556fc96458
Lokesh Rayala <72343510+LokeshRayala@users.noreply.github.com>
2022-08-04 19:16:34 +0530

    Phase-2 Lesson-2 Assisted Practice

---
+++ Phase-2 Assisted Practice/Lesson-2 Assisted Practice/Practice Project-1,2/Set up JDBC Environment/main/java/mySQLConnectivity/DBConfig.java
@@ -0,0 +1,38 @@
+package mySQLConnectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConfig {
+
+	public static Connection getConnection(Properties props) {
+		// TODO Auto-generated method stub
+		
+		Connection connection=null;
+		
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/ecommerce1";
+		String username="root";
+		String password="Lokesh@5d1";
+		
+		
+		try {
+			//load driver
+			Class.forName(driver);
+			//get connection and store it conn  object
+			connection=DriverManager.getConnection(url,username,password);
+				
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return  connection;
+	}
+
+}
