f7c3846f3e27fd3ffc93f918d548b396ccc9a096
Lokesh Rayala <72343510+LokeshRayala@users.noreply.github.com>
2022-08-23 13:18:27 +0530

    Phase-3 Lesson-2 Assisted Practice

---
+++ Phase-3 Assisted Practice/Lesson-2 Assisted Practice/Practice Project-5/HttpsSpringBoot/src/main/java/com/https/HttpsSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.https;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.https")
+public class HttpsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsSpringBootApplication.class, args);
+	}
+
+}
